08ab8f2c79a3e2120dc1cef6813cc7d65e8527eb
Ross Kraybill <rckraybill@gmail.com>
2018-05-28 07:40:40 -0500

    Functional test now checks we can input a to-do item

--- tests/functional_tests.py
+++ tests/functional_tests.py
@@ -1,10 +1,11 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
 import unittest
 
 class NewVisitorTest(unittest.TestCase):
 
     def setUp(self):
-
         self.browser = webdriver.Firefox()
 
     def tearDown(self):
@@ -19,17 +20,39 @@ class NewVisitorTest(unittest.TestCase):
         # She notices the page title and header mention to-do lists
 
         self.assertIn('To-Do', self.browser.title)
+        header_text = self.browser.find_element_by_tag_name('h1').text
         self.fail('Finish the test!')
 
         # She is invited to enter a to-do item straight away
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Ender a to-do item'
+        )
 
 
-# We type "Buy peacock feathers" into a text box (because we like fly fishing)
 
-# When we hit ender, the page updates, and now the page lists:
-# 1: Buy peacock feathers. as an item in a to-do list
+        # We type "Buy peacock feathers" into a text box (because we like fly fishing)
+        inputbox.send_keys('Buy peacock feathers')
 
-# There is sill a text box inviting her to add another item. She enters "Use peacock feathers to make a fly"
+        # When we hit ender, the page updates, and now the page lists:
+        # 1: Buy peacock feathers. as an item in a to-do list
+
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1: Buy peacock feathers' for row in rows)
+        )
+
+
+
+
+        # There is sill a text box inviting her to add another item. She enters "Use peacock feathers to make a fly"
+        self.fail('Finish the test!')         
+
 
 # The page updates again, and now shows both items on the list
 
